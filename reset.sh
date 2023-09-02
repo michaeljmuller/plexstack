9ee0cc8f4ab913052fe3008baf12301ee24ddcd8
Michael Muller <mike@themullers.org>
2023-09-01 18:26:57 -0600

    added script to delete all the configuration and working files (to start fresh)

---
+++ reset.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this script clears out all the configuration and working files for all the containers
+
+thisDir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+function resetDir() {
+    find "$1" ! -name .gitignore -type f -exec rm -fv {} +
+}
+
+resetDir $thisDir/radarr/config
+resetDir $thisDir/radarr/movies
+resetDir $thisDir/sonarr/config
+resetDir $thisDir/sonarr/tv
+resetDir $thisDir/sabnzbd/config
+resetDir $thisDir/sabnzbd/downloads
+resetDir $thisDir/sabnzbd/incomplete-downloads
+
